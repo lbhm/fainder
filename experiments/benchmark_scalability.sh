b91b32b3edf1382701bfbdbba37312ba6ecc69ab
Lennart Behme <44500208+lbhm@users.noreply.github.com>
2024-12-27 10:57:29 +0100

    Minor fix

--- experiments/benchmark_scalability.sh
+++ experiments/benchmark_scalability.sh
@@ -8,10 +8,10 @@ cd "$(git rev-parse --show-toplevel)"
 start_time=$(date +%s)
 
 ### Setup ###
-cp data/"$dataset"/queries/accuracy_benchmark/test-all.zst data/"$dataset"/queries/scalability_benchmark.zst
 dataset="gittables"
 query=("0.1" "lt" "50")
 # nproc=$(nproc)
+cp data/"$dataset"/queries/accuracy_benchmark/test-all.zst data/"$dataset"/queries/scalability_benchmark.zst
 
 for f in 025 050 100 200; do
     cluster-histograms -i data/"$dataset"/histograms_sf"$f".zst -o data/"$dataset"/clusterings/scalability_benchmark/sf"$f".zst -a kmeans -c 125 150 -b 50000 -t quantile --alpha 1 --seed 42 --log-file logs/scalability_benchmark/indexing/"$dataset"-clustering-sf"$f".log
@@ -30,14 +30,14 @@ for f in 025 050 100 200; do
 
         # Query collection
         run-queries -i data/"$dataset"/indices/scalability_benchmark/rebinning-sf"$f".zst -t index -q data/"$dataset"/queries/scalability_benchmark.zst -m recall --log-file logs/scalability_benchmark/execution/"$dataset"-collection-rebinning-single-sf"$f"-"$i".log
-        # run-queries -i data/"$dataset"/indices/scalability_benchmark/rebinning-sf"$f".zst -t index -q data/"$dataset"/queries/scalability_benchmark.zst -m recall -w "$nproc" --log-file logs/scalability_benchmark/execution/"$dataset"-collection-rebinning-parallel-sf"$f"-"$i".log
         run-queries -i data/"$dataset"/indices/scalability_benchmark/conversion-sf"$f".zst -t index -q data/"$dataset"/queries/scalability_benchmark.zst -m recall --log-file logs/scalability_benchmark/execution/"$dataset"-collection-conversion-single-sf"$f"-"$i".log
+        # run-queries -i data/"$dataset"/indices/scalability_benchmark/rebinning-sf"$f".zst -t index -q data/"$dataset"/queries/scalability_benchmark.zst -m recall -w "$nproc" --log-file logs/scalability_benchmark/execution/"$dataset"-collection-rebinning-parallel-sf"$f"-"$i".log
         # run-queries -i data/"$dataset"/indices/scalability_benchmark/conversion-sf"$f".zst -t index -q data/"$dataset"/queries/scalability_benchmark.zst -m recall -w "$nproc" --log-file logs/scalability_benchmark/execution/"$dataset"-collection-conversion-parallel-sf"$f"-"$i".log
 
         # Query collection (without processing results)
         run-queries -i data/"$dataset"/indices/scalability_benchmark/rebinning-sf"$f".zst -t index -q data/"$dataset"/queries/scalability_benchmark.zst -m recall --suppress-results --log-file logs/scalability_benchmark/execution/"$dataset"-collection-rebinning-single_suppressed-sf"$f"-"$i".log
-        # run-queries -i data/"$dataset"/indices/scalability_benchmark/rebinning-sf"$f".zst -t index -q data/"$dataset"/queries/scalability_benchmark.zst -m recall -w"$nproc" --suppress-results --log-file logs/scalability_benchmark/execution/"$dataset"-collection-rebinning-parallel_suppressed-sf"$f"-"$i".log
         run-queries -i data/"$dataset"/indices/scalability_benchmark/conversion-sf"$f".zst -t index -q data/"$dataset"/queries/scalability_benchmark.zst -m recall --suppress-results --log-file logs/scalability_benchmark/execution/"$dataset"-collection-conversion-single_suppressed-sf"$f"-"$i".log
+        # run-queries -i data/"$dataset"/indices/scalability_benchmark/rebinning-sf"$f".zst -t index -q data/"$dataset"/queries/scalability_benchmark.zst -m recall -w"$nproc" --suppress-results --log-file logs/scalability_benchmark/execution/"$dataset"-collection-rebinning-parallel_suppressed-sf"$f"-"$i".log
         # run-queries -i data/"$dataset"/indices/scalability_benchmark/conversion-sf"$f".zst -t index -q data/"$dataset"/queries/scalability_benchmark.zst -m recall -w"$nproc" --suppress-results --log-file logs/scalability_benchmark/execution/"$dataset"-collection-conversion-parallel_suppressed-sf"$f"-"$i".log
     done
 done
